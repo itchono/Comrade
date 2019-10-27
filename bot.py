5940ce729765afc270c6c953e9144bc4c0fd147a
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-10-27 00:27:14 -0400

    Initial version

---
+++ bot.py
@@ -0,0 +1,35 @@
+# bot.py
+import os
+
+import discord
+f = list(open('.env'))
+
+TOKEN = f[0].strip('\n')
+GUILD = f[1].strip('\n')
+
+print(TOKEN)
+print(GUILD)
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        #failsafe against self response
+        print(str(message.author))
+        if (str(message.author) == 'Wahaha#0365' or str(message.author) =='itchono#3597'):
+            await message.channel.send('Henlo')
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})')
+    
+    
+
+client.run(TOKEN)
