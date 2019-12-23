cf1004e0f4ab628e21778d191d40c6378cd62bd5
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-12-23 16:41:17 -0500

    First commmit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Comrade Bot - Serenity Branch
+Comrade Bot - Serenity-Osprey Branch
 
 Full Rewrite of functional components of Comrade to be more robust
 Mingde Yin
@@ -23,7 +23,7 @@ import os
 import dotenv # NOTE - install as "pip install python-dotenv"
 
 # Misc stuff
-import datetime # Time func
+from datetime import datetime # Time func
 
 import asyncio # Dependancy for Discord py
 
@@ -34,8 +34,9 @@ import random
 
 # II: Internal Imports
 import keep_alive
+import comrade_cfg
+import utilitymodules
 
-# III: Core modules
 
 '''
 Initialization Phase
@@ -46,65 +47,129 @@ Initialization Phase
 
 '''
 print('Comrade is currently starting up...')
-t_start = datetime.datetime.utcnow() # start time
+t_start = datetime.utcnow() # start time
 
 # I: Variable Loading
 dotenv.load_dotenv()
 TOKEN = os.environ.get('TOKEN')
 GUILD_ID = os.environ.get('GUILD')
 MONGO_DRIVER = os.environ.get('DB')
-print('Environment variables loaded.')
 
-mongoClient = pymongo.MongoClient(MONGO_DRIVER) # requires dnspython
-cfg = mongoClient["Comrade"]["cfg"]
-
-def writeCFG(key, data, doc = "generalCFG"):
-    '''
-    Updates value in cfg database given a document id, key to modify, and new data value.
-    Defaults to writing in general CFG document
-
-    >>> writeCFG("LETHALITY", 1)
-    changes LETHALITY field under generalCFG to value of 1
-
-    >>> writeCFG(")
-    '''
-    cfg.update_one({"_id":doc}, {"$set":{key:data}})
-
-#writeCFG(308287917556498452, list(list(cfg["kickVotes"][308287917556498452]) + [123456]), doc = "kickVotes")
-
-# Complex Variables
-kickVotes = {}
-for x in (cfg.find({"_id":"kickVotes"})):
-    kickVotes = x
-
-THREATS = {}
-for x in (cfg.find({"_id":"THREATS"})):
-    THREATS = x
-
-print(cfg.kickVotes)
-
-print(kickVotes)
-print(THREATS)
+cfg = comrade_cfg.data # load config variables
 
 # Temporary Variables
 vaultCandidates = {}
 hando_list = {}
 
-
 # II: Client startup
 client = discord.Client()
 bot = commands.Bot(command_prefix="$comrade")
+
+'''
+FUNCTIONS
+'''
+
+def writeInfo():
+    '''
+    Writes changes made to cfg dictionary to configuration file.
+    '''
+    if os.path.exists("comrade_cfg.py"):
+        os.remove("comrade_cfg.py")
+        with open("comrade_cfg.py", "w") as f:
+            f.write("{} = {}\n".format("data", cfg))
+        # announce to log channel
+        await log("Data Written Successfully.")
+    else:
+        await log("Data could not be written.")
+
+def lastDaily():
+    '''
+    returns last_daily in more compact syntax
+    '''
+    return datetime.strptime(cfg["LAST_DAILY"], "%Y-%m-%d")
+
+async def log(msg):
+    '''
+    Logs stuff into preferred log channel.
+    '''
+    await client.get_guild(419214713252216848).get_channel(446457522862161920).send(msg)
+
+async def dailyMSG(force = False):
+    '''
+    Daily routine at 7 AM EST. Used to make announcement and some others.
+    Can be force called.
+    '''
+    await client.wait_until_ready()
+    
+    while not client.is_closed():
+
+        if (datetime.utcnow().date > lastDaily() and datetime.utcnow().hour == 12) or force:
+            await client.get_guild(419214713252216848).get_channel(419214713755402262).send('Good morning everyone!\nToday is {}. Have a prosperous day! <:FeelsProsperousMan:419256328465285131>'.format(datetime.utcnow().date()))
+            cfg["LAST_DAILY"] = str(datetime.utcnow().date())
+            writeInfo()
+            await log("Daily Announcement Made. Current LAST_DAILY = {}".format(cfg["LAST_DAILY"]))
+        await asyncio.sleep(60)
+
+async def setKick():
+    '''
+    Generates/Regenerates kicklist for all members on server
+    '''
+    for member in client.get_guild(419214713252216848).members:
+        cfg["kickVotes"][member.id] = []
+    await log("Kicklist regenerated.")
+
+async def quarantine(user:discord.user):
+    '''
+    (user object)
+
+    Quarantines or unquarantines a user.
+    '''
+    currRoles = user.roles
+    isQ = False
+    for r in currRoles:
+        if r.id == 613106246874038274: # quarantine role
+            isQ = True
+            currRoles.remove(r)
+            # remove the role if quarantined
+        elif r.id == 419215295232868361: # regular role
+            currRoles.remove(r)
+
+async def sentinelFilter(message:discord.message):
+    '''
+    Sentinel message filtering system, allows for multivalent toggles
+    '''
+
+    # Stage 1: text detection
+    query = re.sub('\W+','', unidecode.unidecode(message.content.lower())) # clean message content down to text
+    
+    strict = message.author.id in cfg["THREATS"] and cfg["THREATS"][message.author.id] >= 2
+
+
+    for word in set(cfg["GLOBAL_BANNED_WORDS"]).union(set(cfg["THREATS"][message.author.id] if message.author.id in cfg["THREATS"] else set())):
+        if word in query or (fuzz.partial_ratio(word, query) > 75 and strict):
+            await message.delete()
+            await log('Message purged for bad word:\n'+ str(message.content))
+            
+            #await infraction(message, message.author.id, 0.5)
+
+    # Stage 2: Emoji backfiltering
+
+'''
+COMMANDS
+'''
 
 @bot.event
 async def status(ctx):
     await ctx.send("OK")
 
-
-
 # create server
-keep_alive.keep_alive()
+'''keep_alive.keep_alive()'''
 # create tasks
-#client.loop.create_task(dailyMSG())
+'''client.loop.create_task(dailyMSG())'''
 
 # finally, start the bot
-# client.run(TOKEN)
+'''client.run(TOKEN)'''
+
+if __name__ == "__main__":
+    cfg["LETHALITY"] = 1
+    writeInfo()
