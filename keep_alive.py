465b42ef4dde369f289470cb0bd4286955b970af
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-12-24 12:29:05 -0500

    Update keep_alive.py
    Former-commit-id: 126cb231109c222b9c66a00e56fb5b4c0b64ab5e
    Former-commit-id: b4f81176f6321378d2475f3939bcd88e2e96a6f9

--- keep_alive.py
+++ keep_alive.py
@@ -13,11 +13,7 @@ def main():
 
     # config status
     s = 'Comrade BOT is online\n'
-
     s += "Uptime: {}\n".format(datetime.utcnow() - t_start)
-    s += "Bot Stats:\nThreats: {0}\nKickList: {1}\nOPS: {2}\nLethality: {3}\nKick Requirement: {4}\nLast Daily: {5}\nSafe From Kick: {6}\nUser Banned Words: {7}".format(comrade_cfg.THREATS, 
-    comrade_cfg.kickList, comrade_cfg.OPS, comrade_cfg.LETHALITY, comrade_cfg.KICK_REQ, comrade_cfg.LAST_DAILY, comrade_cfg.KICK_SAFE, comrade_cfg.USER_BANNED_WORDS)
-
     return s
 
 
