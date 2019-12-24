71b00a09d6ab8a9ec5c0723bd0eea542cfa93b0f
Mingde Yin <54449457+itchono@users.noreply.github.com>
2019-12-24 18:06:25 -0500

    Commands almost fully implemented
    Former-commit-id: 6b4668419972c5362e7169e65bd12c22d35c5d1e
    Former-commit-id: 795795580af6f04e4368af9ba5be5c03a707f27d

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from threading import Thread
+from datetime import datetime
+
+t_start = 0
 
 app = Flask('')
 
 @app.route('/')
 def main():
-    return "Comrade BOT is online."
+    # debug info
 
-
+    # config status
+    s = 'Comrade BOT is online\n'
+    s += "Uptime: {}\n".format(datetime.utcnow() - t_start)
+    return s
 
 def run():
+    global t_start
+    t_start = datetime.utcnow()
     app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     server = Thread(target=run)
     server.start()
+
+def shutdown():
+    server = Thread(target=run)
+    server.stop()
