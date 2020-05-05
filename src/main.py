622a5a320eb0c549d999690054b7833997335ba4
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-05-05 01:48:13 -0400

    Update main.py
    Former-commit-id: 8c93dceb8ba587e03aaa20fd267a595241f8240d
    Former-commit-id: db16e4d6178dbc270a22b70a9c47b443766dca06

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ April - May 2020
 import os
 import dotenv
 
+import asyncio
+
 import discord # core to bot
 from discord.ext import commands
 
@@ -24,11 +26,17 @@ dotenv.load_dotenv()
 TOKEN = os.environ.get('TOKEN') # bot token; kept private
 client = commands.Bot(command_prefix="$c ") # declare bot with prefix $c
 
+@client.command
+async def memeapproved(ctx):
+    await ctx.send("Meme Approved")
+
 @client.event
 async def on_message(message:discord.message):
     if message.author != client.user:
         if "hello" in message.content.lower():
-            await message.channel.send("Henlo")
+            msg = await message.channel.send("Henlo")
+            await asyncio.sleep(5)
+            await msg.delete()
 
 print("Bot is online")
 client.run(TOKEN)
