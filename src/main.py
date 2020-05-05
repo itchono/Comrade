593bf00eb8bc022163e6ffae3e9dcd83279748a0
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-05-05 01:23:42 -0400

    Update main.py
    Former-commit-id: fec476b67c571fdc3304b3c8d2b2c84a3f5ecc4f
    Former-commit-id: 75c49f8adf83a92a0e67b1ddda42c039e79ff1e7

--- src/main.py
+++ src/main.py
@@ -7,11 +7,33 @@ Mingde Yin
 April - May 2020
 
 '''
+import os
+import dotenv
+
+import discord # core to bot
+from discord.ext import commands
+
 
 '''
 VARIABLES
 
 '''
 
-if __name__ == "__main__":
-    print("Comrade V3 Starting...")
+print("Comrade V3 Starting...")
+dotenv.load_dotenv()
+TOKEN = os.environ.get('TOKEN') # bot token; kept private
+client = commands.Bot(command_prefix="$c ") # declare bot with prefix $c
+
+@client.event
+async def on_message(message:discord.message):
+    if message.author != client.user:
+        if "hello" in message.content.lower():
+            await message.channel.send("Henlo")
+
+print("Bot is online")
+client.run(TOKEN)
+
+
+
+
+
