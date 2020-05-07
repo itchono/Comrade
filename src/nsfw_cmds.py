cc432ee46acfb8741e3e90cac62dee3e0c1f136e
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-05-07 15:14:29 -0400

    Update nsfw_cmds.py

--- src/nsfw_cmds.py
+++ src/nsfw_cmds.py
@@ -11,21 +11,30 @@ class NSFW(commands.Cog):
         self._last_member = None
 
     @commands.command()
-    async def hentai(self, ctx:commands.Context, tags:str, num:int):
-        print("working...")
+    async def hentai(self, ctx:commands.Context, tags:str, num:int = 1):
+        '''
+        Fetches a number of posts from Danbooru given a series of tags. Input with quotes.
+        Ex. $c hentai "arms_up solo" 2
 
-        if ctx.channel.id == getCFG(ctx.guild.id)["hentai channel"]:
-            client = Danbooru('danbooru')
-            
-            posts = client.post_list(tags=tags, limit=num)
+        User $c hentai clear to purge all hentai messages
+        '''
+        if tags.lower() == "clear":
 
-            if not posts:
-                await ctx.send("No results found. Please try another tag.")
-            else:
-                for post in posts:
-                    e = discord.Embed(title=tags, url=post["file_url"])
-                    e.set_image(url=post["file_url"])
-                    print("sending...")
-                    await ctx.send(embed=e)
         else:
-            await delSend("Please use this command in the hentai channel.", ctx.channel)
+            if ctx.channel.id == getCFG(ctx.guild.id)["hentai channel"]:
+                client = Danbooru('danbooru')
+                
+                posts = client.post_list(tags=tags, limit=num, random=True)
+
+                if not posts:
+                    await ctx.send("No results found. Please try another tag.")
+                else:
+                    for post in posts:
+                        try:
+                            e = discord.Embed(title=tags, url=post["file_url"])
+                            e.set_image(url=post["file_url"])
+                            await ctx.send(embed=e)
+                        except:
+                            e = discord.Embed(title=tags, description="Unknown Image Format, Could not be embedded.")
+            else:
+                await delSend("Please use this command in the hentai channel.", ctx.channel)
