b916916383290d4a842dbc514e0ae62d3b73bad1
slyflare <vimal.g.132000@gmail.com>
2020-05-07 15:59:22 -0400

    Update general_cmds.py
    Former-commit-id: 18569c046aa67ef364add5428c5d63e1c9f2fdd0
    Former-commit-id: 65c8b93aa321ed47a7aa592301a6e2e6dbb43654

--- src/general_cmds.py
+++ src/general_cmds.py
@@ -69,6 +69,8 @@ class General(commands.Cog):
         embed.set_author(name=f"User Info - {member}")
         embed.set_thumbnail(url=member.avatar_url)
         embed.set_footer(icon_url=ctx.author.avatar_url)
-        embed.add_field(name=f'Roles: ({len(roles)})')
+        embed.add_field(name=f'Roles: ({len(roles)})', value="".join([role for role in member.roles]))
+
+        await ctx.send(embed=embed)
 
 
