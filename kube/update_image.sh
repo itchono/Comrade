7c32136c4f43dc205d7eaa404758fb9ced855718
Sean D'Souza <sdammobubbles@gmail.com>
2020-05-22 20:23:13 -0400

    Update script to deploy v2 and v3 simultaneously
    Former-commit-id: b0d5c2ab3d5cd0520db6a2e4fc58887edf13a239
    Former-commit-id: aeb61d263869880a9649011277498ae5bd75c2c1

--- kube/update_image.sh
+++ kube/update_image.sh
@@ -4,6 +4,7 @@ QUERY=`curl https://api.github.com/repos/itchono/Comrade/actions/runs 2> /dev/nu
 
 update_deploy () {
   local BRANCH=$1
+  local UPDATE_FILE=$2
   
   # status completed and conclusion success and right branch
   local COMMIT=`echo $QUERY | jq --arg branch "$BRANCH" -r '[.workflow_runs[] | select((.status == "completed") and (.conclusion == "success") and (.head_branch == $branch)).head_sha][0]'`
@@ -26,13 +27,13 @@ update_deploy () {
     echo "Updating $BRANCH image."
     echo $COMMIT > $FILE
   
-    local KUBE_FILE=$DIR/pod.yaml
+    local KUBE_FILE=$DIR/$UPDATE_FILE
   
     sed -i "s/\\(itchono\/comrade:\\).*/\\1$TAG/" "$KUBE_FILE"
-    kubectl apply -f $KUBE_FILE
+    #kubectl apply -f $KUBE_FILE
   fi
 }
 
-update_deploy "master"
-update_deploy "Comrade-V3-dev"
+update_deploy "master" "v2/pod.yaml"
+update_deploy "Comrade-V3-dev" "v3/deploy.yaml"
 
