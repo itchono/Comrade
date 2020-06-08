afdfa15c7e020f656a029187eb3081f307e4e2ae
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-06-07 21:11:13 -0400

    Advanced self-ping system added
    Used instead of uptime robot

--- src/utils/keep_alive.py
+++ src/utils/keep_alive.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 from threading import Thread
-from datetime import datetime
+from utils.utilities import *
 
 import logging
 import os
+
+import requests
+import json
+import aiohttp
+import urllib.request
 
 logging.getLogger('werkzeug').disabled = True
 os.environ['WERKZEUG_RUN_MAIN'] = 'true'
@@ -15,13 +20,13 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return 'Comrade BOT is online - Uptime: {}'.format(datetime.utcnow() -
+    return 'Comrade is online - Uptime: {}'.format(localTime() -
                                                        t_start)
 
 
 def run():
     global t_start
-    t_start = datetime.utcnow()
+    t_start = localTime()
     app.run(host="0.0.0.0", port=8080)
 
 
@@ -33,3 +38,43 @@ def keep_alive():
 def shutdown():
     server = Thread(target=run)
     server._stop()
+
+# for repl.it
+class SelfPing(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.lastping = None
+        self._last_member = None
+
+        self.selfping.start()
+
+    def cog_unload(self):
+        self.selfping.cancel()
+
+    @tasks.loop(minutes=1.0)
+    async def selfping(self):
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers={'User-Agent':user_agent}
+        url = 'https://comrade--itchono.repl.co/'
+
+        try:
+            request = urllib.request.Request(url,None,headers)
+
+            response = urllib.request.urlopen(request)
+            data = response.read()
+            print(data)
+
+            self.lastping = localTime()
+        except:
+            print("ERROR pinging self!")
+
+    @commands.command(name="lastping")
+    async def callping(self, ctx : commands.Context):
+        await ctx.send("Last ping was at {}".format(self.lastping))
+
+
+    @selfping.before_loop
+    async def before_selfping(self):
+        await self.bot.wait_until_ready()
+        print("Self ping routine started.")
+
