fbde94e056738bed4582b3f49c9bcd4ce7910a2e
Mingde Yin <mingdeyin@gmail.com>
2020-08-03 20:30:48 -0400

    Trimmed the code
    Added changes

--- src/commands/general_cmds.py
+++ src/commands/general_cmds.py
@@ -122,27 +122,27 @@ class General(commands.Cog):
         BORDER_TOP =    "╔═══.·:·.✧    ✦    ✧.·:·.═══╗"
         ACCENT_BORDER = "      ≻───── ⋆✩⋆ ─────≺"
         BORDER_BOTTOM = "╚═══.·:·.✧    ✦    ✧.·:·.═══╝"
+        len_border = len(BORDER_TOP)
 
         words = content.split(" ")
-        len_border = len(BORDER_TOP)
         lines = []
-        buffer = ""
+        buffer = "" # line buffer
 
         while words:
-            if len(words[0]) > len_border:
-                    max_word = words.pop(0)
-                    split = [max_word[:len_border-2] + '-', max_word[len_border-2:]]
-                    words = split + words
-            while words and len(buffer + words[0]) < len(BORDER_TOP):
-                    buffer += words.pop(0) + " "
-            buffer = buffer.center(len(BORDER_TOP))
-            lines.append(buffer)
+            # do until the array of words is empty
+
+            if len(words[0]) >= len_border and (max_word := words.pop(0)): words = [max_word[:len_border-2] + '-', max_word[len_border-2:]] + words
+
+            while words and len(buffer + words[0]) < len_border: buffer += words.pop(0) + " "
+            
+            lines.append(buffer.strip(" ").center(len_border)) # center the text in the block after removing spaces
             buffer = ""
                        
         content = "\n".join(lines)
 
         c = self.bot.get_cog("Echo")
 
+        # using monospaced font to fix spacing
         await c.echo(ctx, f"**```{BORDER_TOP}\n{ACCENT_BORDER}\n{content}\n{ACCENT_BORDER}\n{BORDER_BOTTOM}```**", str(ctx.author.id))
 
 
