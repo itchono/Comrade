86d28c4eba63591c87567d9c998a87a6cf3a4075
Mingde Yin <mingdeyin@gmail.com>
2020-08-09 20:53:43 -0400

    Update waifu_cmds.py

--- src/commands/waifu_cmds.py
+++ src/commands/waifu_cmds.py
@@ -1,8 +1,15 @@
 from utils.utilities import *
+import random
+
+NAMES = ["Apple", "Banana", "Cow", "DNA", "E"]
 
 class Waifu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.active_waifu = None
+
+        self.produce_waifu.start()
 
     @commands.command()
     async def claim(self, ctx:commands.Context,*, waifuname):
@@ -11,3 +18,31 @@ class Waifu(commands.Cog):
         '''
         await ctx.send(f"Congrats {ctx.author.mention}, you claimed **{waifuname}**!")
         
+    '''
+    Timer based thing
+    '''
+
+    @tasks.loop(seconds=5)
+    async def produce_waifu(self):
+
+        waifu = random.choice(NAMES)
+
+        self.active_waifu = waifu
+
+        '''iraq_btw = self.bot.get_guild(419214713252216848)
+
+        west_korea = iraq_btw.get_channel(522428899184082945)'''
+
+
+        # NOTE: This is just for bot development; NOT when we actually deploy
+        test_btw = self.bot.get_guild(709954286376976425)
+        west_korea = test_btw.get_channel(742181992824700989)
+
+        await west_korea.send(f"Yo a waifu spawned: {waifu}")
+
+    @produce_waifu.before_loop
+    async def before(self):
+        await self.bot.wait_until_ready()
+
+
+
