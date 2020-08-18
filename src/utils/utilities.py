2c6838a27e6be931e95416731cf871af6553ac81
Mingde Yin <mingdeyin@gmail.com>
2020-08-17 22:05:54 -0400

    Added features
    Message triggers are now dynamic and can be added freely
    
    Renamed databases a bit. Will need some reworking before deploying.
    
    Added a way to set channels without as much pain

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -324,15 +324,15 @@ def updateDBuser(userdata):
     
 
 ### NAMES OF EACH DB COLLECTION ###
-USER_COL = "UserData"
-SERVERCFG_COL = "cfg"
-CUSTOMUSER_COL = "CustomUsers"
+USER_COL = "user_data"
+SERVERCFG_COL = "server_configs"
+CUSTOMUSER_COL = "custom_users"
 ANNOUNCEMENTS_COL = "announcements"
-CMD_COL = "CustomCommands"
-LIST_COL = "CustomLists"
-CACHE_COL = "ChannelCache"
-FAVOURITES_COL = "favourites"
-PNG_COL = "pngs"
+CMD_COL = "custom_commands"
+LIST_COL = "custom_lists"
+CACHE_COL = "channel_cache"
+FAVOURITES_COL = "favourite_h_images"
+PNG_COL = "claimed_pngs"
 
 '''
 Misc
