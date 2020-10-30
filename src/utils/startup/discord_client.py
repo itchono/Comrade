f0535294bdf8101ed69f8f6bbdf7739cebc55e4f
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-10-30 18:20:23 -0400

    Update bot to fix broken functionality
    Former-commit-id: b2dc3fda1f75bfd98832d46c51071af982877b28
    Former-commit-id: 49d10e4c8ab1b4764536fa0a99297c478d18617e

--- src/utils/startup/discord_client.py
+++ src/utils/startup/discord_client.py
@@ -4,7 +4,8 @@ from cfg import *
 
 prefixes = (BOT_PREFIX, SECONDARY_PREFIX) if SECONDARY_PREFIX else (BOT_PREFIX,)
 
+intents = discord.Intents.all()
 # Bot Client
 client = commands.Bot(command_prefix=prefixes, case_insensitive=True,
                       help_command=commands.MinimalHelpCommand(
-                          no_category="Help Command"))
+                          no_category="Help Command"), intents=intents)
