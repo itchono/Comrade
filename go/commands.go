aa0dee0fd8e74fc593ca9ac219bebf57e2565b56
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-11-20 23:44:52 -0500

    H functions started in Go

--- go/commands.go
+++ go/commands.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -27,6 +29,47 @@ func Command(s *discordgo.Session, m *discordgo.MessageCreate) int {
 		}
 
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Guild name is %s", g.Name))
+
+	case "hentai":
+
+		channel, _ := s.Channel(m.ChannelID)
+
+		if channel.NSFW {
+			var hdata interface{}
+
+			resp, err := http.Get("https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=1&tags=-rating%3asafe+-webm+-gif+sort%3arandom+")
+			if err != nil {
+				// handle error
+				return -1
+			}
+			defer resp.Body.Close()
+
+			err = json.NewDecoder(resp.Body).Decode(&hdata)
+
+			if err != nil {
+				// handle error
+				return -1
+			}
+
+			hdatalist, ok := hdata.([]interface{}) // decode the outer layer of JSON
+
+			if !ok {
+				return -1
+			}
+
+			postData, ok := hdatalist[0].(map[string]interface{}) // type assert as dictionary
+
+			url := postData["file_url"].(string) // get URL
+
+			emb := discordgo.MessageEmbed{}
+
+			emb.Image = &discordgo.MessageEmbedImage{URL: url}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, &emb)
+
+		} else {
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s is not NSFW!", channel.Mention()))
+		}
 
 	}
 
