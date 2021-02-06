314ffd6003463b7b6fff6bd58703db05d7e6f93c
Mingde Yin <mingdeyin@gmail.com>
2021-02-05 23:11:44 -0500

    Update slash_cmds.py

--- src/Comrade/components/general/slash_cmds.py
+++ src/Comrade/components/general/slash_cmds.py
@@ -13,10 +13,10 @@ class Slash(commands.Cog):
     def __init__(self, bot):
         if not hasattr(bot, "slash"):
             # Creates new SlashCommand instance to bot if bot doesn't have.
-            bot.slash = SlashCommand(bot, override_type=True)
+            bot.slash = SlashCommand(bot, override_type=True, auto_register=True, auto_delete=True)
         self.bot = bot
         self.bot.slash.get_cog_commands(self)
 
-    @cog_ext.cog_slash(name="test")
+    @cog_ext.cog_slash(name="test", guild_ids=guild_ids)
     async def _test(self, ctx: SlashContext):
         await ctx.send("hello")
