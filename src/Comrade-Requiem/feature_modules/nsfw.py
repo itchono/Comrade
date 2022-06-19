ee7323d3711842168c36fb655e4c06e27bebdfdf
Mingde Yin <mingdeyin@gmail.com>
2022-06-19 19:20:48 -0400

    Add NHentai processor to Python side, with cloudflare bypass

--- src/Comrade-Requiem/feature_modules/nsfw.py
+++ src/Comrade-Requiem/feature_modules/nsfw.py
@@ -1,34 +1,37 @@
 from dis_snek.models.snek import (Scale, slash_command,
-                                     OptionTypes, slash_option,
-                                     SlashCommandChoice,
-                                     ComponentCommand, InteractionContext,
-                                     ComponentContext)
+                                  OptionTypes, slash_option,
+                                  SlashCommandChoice,
+                                  ComponentCommand, InteractionContext,
+                                  ComponentContext)
 from dis_snek.models.discord import Button, ButtonStyles
 from logger import log
 
-from processors import gelbooru_api, e621_api
+from processors import gelbooru_api, e621_api, nhentai_processor
 
 
 gelbooru_sessions = {}
 genderbend_sessions = {}
+nhentai_sessions = {}
+nsearch_sessions = {}
 
 
 async def e621_button_next(ctx: ComponentContext):
     # Disable the previous button
     await ctx.edit_origin(components=[])
-    
+
     if ctx.channel.id not in genderbend_sessions:
         await ctx.channel.send(
             "Sorry, the session attached to this button has expired.",
-            reply_to = ctx.message)
-    
+            reply_to=ctx.message)
+
     session = genderbend_sessions[ctx.channel.id]
-    
+
     if data := await e621_api.e621_get_next_post_data(session):
         embeds = [e621_api.e621_formatted_embed(post, session)
-            for post in data["posts"]]
-        
-        next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="e621_next")
+                  for post in data["posts"]]
+
+        next_button = Button(style=ButtonStyles.GREY,
+                             label="Next", emoji="🔽", custom_id="e621_next")
         await ctx.channel.send(embeds=embeds, components=[next_button])
     else:
         await ctx.send("No more posts.")
@@ -37,19 +40,20 @@ async def e621_button_next(ctx: ComponentContext):
 async def gb_button_next(ctx: ComponentContext):
     # Disable the previous button
     await ctx.edit_origin(components=[])
-    
+
     if ctx.channel.id not in genderbend_sessions:
         await ctx.channel.send(
             "Sorry, the session attached to this button has expired.",
-            reply_to = ctx.message)
-    
+            reply_to=ctx.message)
+
     session = genderbend_sessions[ctx.channel.id]
-    
+
     if data := await e621_api.gb_get_next_post_data(session):
         embeds = [e621_api.gb_formatted_embed(post, session)
-            for post in data["posts"]]
-        
-        next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="gb_next")
+                  for post in data["posts"]]
+
+        next_button = Button(style=ButtonStyles.GREY,
+                             label="Next", emoji="🔽", custom_id="gb_next")
         await ctx.channel.send(embeds=embeds, components=[next_button])
     else:
         await ctx.send("No more posts.")
@@ -58,24 +62,137 @@ async def gb_button_next(ctx: ComponentContext):
 async def hentai_button_next(ctx: ComponentContext):
     # Disable the previous button
     await ctx.edit_origin(components=[])
-    
+
     if ctx.channel.id not in gelbooru_sessions:
         await ctx.channel.send(
             "Sorry, the session attached to this button has expired.",
-            reply_to = ctx.message)
-    
+            reply_to=ctx.message)
+
     session = gelbooru_sessions[ctx.channel.id]
-    
+
     if data := await gelbooru_api.get_next_post_data(session):
         attrs = data["@attributes"]
         embeds = [gelbooru_api.formatted_embed(post, attrs, session)
-            for post in data["post"]]
-        
-        next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="hentai_next")
+                  for post in data["post"]]
+
+        next_button = Button(style=ButtonStyles.GREY,
+                             label="Next", emoji="🔽", custom_id="hentai_next")
         await ctx.channel.send(embeds=embeds, components=[next_button])
     else:
         await ctx.send("No more posts.")
- 
+
+
+async def nhentai_button_next(ctx: ComponentContext):
+    # Disable the previous button
+    await ctx.edit_origin(components=[])
+
+    if ctx.channel.id not in nhentai_sessions:
+        await ctx.channel.send(
+            "Sorry, the session attached to this button has expired.",
+            reply_to=ctx.message)
+
+    nhentai_sessions[ctx.channel.id].current_page_number += 1
+    session = nhentai_sessions[ctx.channel.id]
+
+    if session.current_page_number > session.length:
+        await ctx.send("No more pages.")
+        return
+
+    next_button = Button(style=ButtonStyles.GREY,
+                         label="Next", emoji="🔽", custom_id="nhentai_next")
+
+    # Simply get the URL of the image for the next message
+    await ctx.channel.send(session.pages[session.current_page_number].direct_url,
+                           components=[next_button])
+
+
+async def nsearch_button_next(ctx: ComponentContext):
+    if ctx.channel.id not in nsearch_sessions:
+        await ctx.channel.send(
+            "Sorry, the session attached to this button has expired.",
+            reply_to=ctx.message)
+
+    session = nsearch_sessions[ctx.channel.id]
+
+    # Advance cursor by one position
+    current_pg_num_results = len(session.cached_pages[session.page_cursor])
+
+    if session.page_subcursor + 1 >= current_pg_num_results:
+        session.page_cursor += 1
+        session.page_subcursor = 0
+
+        if session.page_cursor >= session.num_pages:
+            await ctx.send("No more pages.")
+            return
+    else:
+        session.page_subcursor += 1
+
+    # Get first page result
+    nhentai_session = nhentai_processor.nhentai_search_cursor(session)
+    embed = nhentai_processor.cover_page_embed(nhentai_session)
+
+    page_len = len(session.cached_pages[session.page_cursor])
+    position_indicator = f"Results Page: {session.page_cursor+1}/{session.num_pages}; Result: {session.page_subcursor+1}/{page_len}"
+
+    await ctx.edit_origin(content=position_indicator, embeds=[embed])
+
+    # update session
+    nsearch_sessions[ctx.channel.id] = session
+
+
+async def nsearch_button_prev(ctx: ComponentContext):
+    if ctx.channel.id not in nsearch_sessions:
+        await ctx.channel.send(
+            "Sorry, the session attached to this button has expired.",
+            reply_to=ctx.message)
+
+    session = nsearch_sessions[ctx.channel.id]
+
+    # Advance cursor by one position
+    current_pg_num_results = len(session.cached_pages[session.page_cursor])
+
+    if session.page_subcursor == 0:
+        if session.page_cursor == 0:
+            await ctx.send("This is the first page!.")
+            return
+
+        session.page_cursor -= 1
+
+        pg_num_results = len(session.cached_pages[session.page_cursor])
+        session.page_subcursor = pg_num_results - 1
+    else:
+        session.page_subcursor -= 1
+
+    # Get first page result
+    nhentai_session = nhentai_processor.nhentai_search_cursor(session)
+    embed = nhentai_processor.cover_page_embed(nhentai_session)
+
+    page_len = len(session.cached_pages[session.page_cursor])
+    position_indicator = f"Results Page: {session.page_cursor+1}/{session.num_pages}; Result: {session.page_subcursor+1}/{page_len}"
+
+    await ctx.edit_origin(content=position_indicator, embeds=[embed])
+
+    # update session
+    nsearch_sessions[ctx.channel.id] = session
+
+
+async def nsearch_button_exec(ctx: ComponentContext):
+    if ctx.channel.id not in nsearch_sessions:
+        await ctx.channel.send(
+            "Sorry, the session attached to this button has expired.",
+            reply_to=ctx.message)
+
+    search_session = nsearch_sessions[ctx.channel.id]
+    session = nhentai_processor.nhentai_search_cursor(search_session)
+    nhentai_sessions[ctx.channel.id] = session
+
+    next_button = Button(style=ButtonStyles.GREY,
+                         label="Next", emoji="🔽", custom_id="nhentai_next")
+    embed = nhentai_processor.cover_page_embed(session)
+
+    await ctx.send(
+        embeds=[embed], components=[next_button])
+
 
 class NSFW(Scale):
     @slash_command(name="hentai",
@@ -87,8 +204,7 @@ class NSFW(Scale):
                   opt_type=OptionTypes.INTEGER, required=False,
                   min_value=1, max_value=20)
     @slash_option(name="sorting", description="Sorting method, default random",
-                  opt_type=OptionTypes.STRING, choices=
-                  [
+                  opt_type=OptionTypes.STRING, choices=[
                       SlashCommandChoice(name="random", value="random:0"),
                       SlashCommandChoice(name="highest score", value="score"),
                       SlashCommandChoice(name="most recent", value="id")
@@ -98,29 +214,30 @@ class NSFW(Scale):
         if tags:
             # Clean up tags such that they're joined by +
             tags = "+".join(tags.split())
-        
+
         # Save Session
         gelbooru_sessions[ctx.channel.id] = gelbooru_api.GelbooruSession(
             tags if tags else "", count, sorting, 0)
-        
+
         if data := await gelbooru_api.get_next_post_data(gelbooru_sessions[ctx.channel.id]):
             attrs = data["@attributes"]
             embeds = [
-                gelbooru_api.formatted_embed(post, attrs, gelbooru_sessions[ctx.channel.id])
+                gelbooru_api.formatted_embed(
+                    post, attrs, gelbooru_sessions[ctx.channel.id])
                 for post in data["post"]]
-            
+
             video_urls = [
                 post["file_url"] for post in data["post"]
-                if ("webm" in post["file_url"] or "mp4" in post["file_url"]) ]
-            
-            next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="hentai_next")
+                if ("webm" in post["file_url"] or "mp4" in post["file_url"])]
+
+            next_button = Button(
+                style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="hentai_next")
             await ctx.send(
                 embeds=embeds, components=[next_button],
                 content="\n".join(video_urls) if video_urls else None)
         else:
             await ctx.send("No posts were found.")
-            
-            
+
     @slash_command(name="genderbend",
                    description="Retrieves a post from genderbend.me.",
                    scopes=[419214713252216848, 709954286376976425])
@@ -130,8 +247,7 @@ class NSFW(Scale):
                   opt_type=OptionTypes.INTEGER, required=False,
                   min_value=1, max_value=20)
     @slash_option(name="sorting", description="Sorting method, default random",
-                  opt_type=OptionTypes.STRING, choices=
-                  [
+                  opt_type=OptionTypes.STRING, choices=[
                       SlashCommandChoice(name="random", value="random"),
                       SlashCommandChoice(name="highest score", value="score"),
                       SlashCommandChoice(name="most recent", value="id")
@@ -141,21 +257,23 @@ class NSFW(Scale):
         if tags:
             # Clean up tags such that they're joined by +
             tags = "+".join(tags.split())
-        
+
         # Save Session
         genderbend_sessions[ctx.channel.id] = e621_api.GBSession(
             tags if tags else "", count, sorting, 0)
-        
+
         if data := await e621_api.gb_get_next_post_data(genderbend_sessions[ctx.channel.id]):
             embeds = [
-                e621_api.gb_formatted_embed(post, genderbend_sessions[ctx.channel.id])
+                e621_api.gb_formatted_embed(
+                    post, genderbend_sessions[ctx.channel.id])
                 for post in data["posts"]]
-            
-            next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="gb_next")
+
+            next_button = Button(style=ButtonStyles.GREY,
+                                 label="Next", emoji="🔽", custom_id="gb_next")
             await ctx.send(embeds=embeds, components=[next_button])
         else:
             await ctx.send("No posts were found.")
-            
+
     @slash_command(name="e621",
                    description="Retrieves a post from e621.net.",
                    scopes=[419214713252216848, 709954286376976425])
@@ -165,8 +283,7 @@ class NSFW(Scale):
                   opt_type=OptionTypes.INTEGER, required=False,
                   min_value=1, max_value=20)
     @slash_option(name="sorting", description="Sorting method, default random",
-                  opt_type=OptionTypes.STRING, choices=
-                  [
+                  opt_type=OptionTypes.STRING, choices=[
                       SlashCommandChoice(name="random", value="random"),
                       SlashCommandChoice(name="highest score", value="score"),
                       SlashCommandChoice(name="most recent", value="id")
@@ -176,43 +293,134 @@ class NSFW(Scale):
         if tags:
             # Clean up tags such that they're joined by +
             tags = "+".join(tags.split())
-        
+
         # Save Session
         genderbend_sessions[ctx.channel.id] = e621_api.GBSession(
             tags if tags else "", count, sorting, 0)
-        
+
         if data := await e621_api.e621_get_next_post_data(genderbend_sessions[ctx.channel.id]):
             embeds = [
-                e621_api.e621_formatted_embed(post, genderbend_sessions[ctx.channel.id])
+                e621_api.e621_formatted_embed(
+                    post, genderbend_sessions[ctx.channel.id])
                 for post in data["posts"]]
-            
-            next_button = Button(style=ButtonStyles.GREY, label="Next", emoji="🔽", custom_id="e621_next")
+
+            next_button = Button(style=ButtonStyles.GREY,
+                                 label="Next", emoji="🔽", custom_id="e621_next")
             await ctx.send(embeds=embeds, components=[next_button])
         else:
             await ctx.send("No posts were found.")
-        
+
+    @slash_command(name="nhentai",
+                   description="Retrieves a gallery from NHentai.",
+                   scopes=[419214713252216848, 709954286376976425])
+    @slash_option(name="gallery_id", description="ID of the gallery.",
+                  opt_type=OptionTypes.INTEGER, required=True)
+    async def nhentai(self, ctx: InteractionContext, gallery_id: int):
+        await ctx.defer()
+
+        # Save Session
+        session = nhentai_processor.initialize_session(gallery_id)
+        nhentai_sessions[ctx.channel.id] = session
+
+        if not session.exists:
+            await ctx.send(f"No result could be returned. Error: {session.error}")
+            return
+
+        next_button = Button(style=ButtonStyles.GREY,
+                             label="Next", emoji="🔽", custom_id="nhentai_next")
+        embed = nhentai_processor.cover_page_embed(session)
+
+        await ctx.send(
+            embeds=[embed], components=[next_button])
+
+    @slash_command(name="nsearch",
+                   description="Searches NHentai for a given query.",
+                   scopes=[419214713252216848, 709954286376976425])
+    @slash_option(name="query", description="tags to search for",
+                  opt_type=OptionTypes.STRING, required=False)
+    @slash_option(name="sorting", description="Sorting method, default popular",
+                  opt_type=OptionTypes.STRING, choices=[
+                      SlashCommandChoice(name="recent", value="recent"),
+                      SlashCommandChoice(name="popular", value="popular")
+                  ], required=False)
+    async def nsearch(self, ctx: InteractionContext, query: str = None, sorting: str = "popular"):
+        await ctx.defer()
+
+        # Save Session
+        session = nhentai_processor.nhentai_search(query, sorting)
+        nsearch_sessions[ctx.channel.id] = session
+
+        if session.num_results == 0:
+            await ctx.send(f"No result could be returned. Error: {session.error}")
+            return
+
+        prev_button = Button(style=ButtonStyles.GREY,
+                             label="Prev Result", emoji="◀", custom_id="nsearch_prev")
+        exec_button = Button(style=ButtonStyles.GREY,
+                             label="View Gallery", emoji="🔽", custom_id="nsearch_exec")
+        next_button = Button(style=ButtonStyles.GREY,
+                             label="Next Result", emoji="▶", custom_id="nsearch_next")
+
+        # Get first page result
+        nhentai_session = nhentai_processor.nhentai_search_cursor(session)
+        embed = nhentai_processor.cover_page_embed(nhentai_session)
+
+        page_len = len(session.cached_pages[session.page_cursor])
+        position_indicator = f"Results Page: 1/{session.num_pages}; Result: 1/{page_len}"
+
+        await ctx.send(position_indicator,
+                       embeds=[embed], components=[prev_button, exec_button, next_button])
+
 
 def setup(bot):
     NSFW(bot)
     bot.add_component_callback(
         ComponentCommand(
             name="ComponentCallback::hentai_next",
-            callback = hentai_button_next,
+            callback=hentai_button_next,
             listeners=["hentai_next"]
         )
     )
     bot.add_component_callback(
         ComponentCommand(
             name="ComponentCallback::gb_next",
-            callback = gb_button_next,
+            callback=gb_button_next,
             listeners=["gb_next"]
         )
     )
     bot.add_component_callback(
         ComponentCommand(
             name="ComponentCallback::e621_next",
-            callback = e621_button_next,
+            callback=e621_button_next,
             listeners=["e621_next"]
+        )
+    )
+    bot.add_component_callback(
+        ComponentCommand(
+            name="ComponentCallback::nhentai_next",
+            callback=nhentai_button_next,
+            listeners=["nhentai_next"]
+        )
+    )
+    bot.add_component_callback(
+        ComponentCommand(
+            name="ComponentCallback::nsearch_next",
+            callback=nsearch_button_next,
+            listeners=["nsearch_next"]
+        )
+    )
+    bot.add_component_callback(
+        ComponentCommand(
+            name="ComponentCallback::nsearch_prev",
+            callback=nsearch_button_prev,
+            listeners=["nsearch_prev"]
+        )
+    )
+    bot.add_component_callback(
+        ComponentCommand(
+            name="ComponentCallback::nsearch_exec",
+            callback=nsearch_button_exec,
+            listeners=["nsearch_exec"]
         )
     )
     log.info("Module nsfw.py loaded.")
---
+++ src/Comrade-Requiem/processors/nhentai_processor.py
@@ -0,0 +1,171 @@
+from turtle import st
+import cloudscraper
+from bs4 import BeautifulSoup
+import re
+from dis_snek.models.discord import Embed
+from dataclasses import dataclass
+
+
+@dataclass
+class NHentaiSearchResult:
+    query: str
+    sort_suffix: str
+    num_results: int
+    num_pages: int
+    cached_pages: "dict[int, list]"
+    # Caches gallery numbers for each page of results
+    page_cursor: int = 0
+    page_subcursor: int = 0
+    # Cursor position tracks which result we're on in the search results
+    error: Exception = None
+
+
+@dataclass
+class NHentaiSession:
+    # User inputs
+    gallery_id: int  # Gallery ID
+
+    # State variables
+    title: str = None
+    gallery_internal_id: int = 0  # Gallery internal ID
+    exists: bool = True  # Whether this gallery exists or not
+    current_page_number: int = 0  # Page number
+    length: int = 0  # Length of gallery
+    pages: "list[NHentaiPage]" = None
+    error: Exception = None  # Error if one occurred
+
+
+@dataclass
+class NHentaiPage:
+    direct_url: str
+    file_extension: str
+
+
+def initialize_session(gallery_id: int) -> NHentaiSession:
+    '''
+    Initialize session using a gallery ID
+    '''
+    scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+
+    query_url = f"https://nhentai.net/g/{gallery_id}/"
+
+    try:
+        result = scraper.get(query_url)
+    except Exception as e:
+        return NHentaiSession(gallery_id=gallery_id, exists=False, error=e)
+
+    # Parse the result using BeautifulSoup
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    # The title will be the third <meta> tag on the page.
+    title = soup.find_all("meta")[2]["content"]
+
+    # The internal gallery ID is found using the fourth <meta> tag on the page.
+    # The link has format https://t.nhentai.net/galleries/######/cover.<EXT>
+    # Extract the number from the link using regex
+    internal_gallery_id = int(re.search(
+        r"galleries/(\d+)/cover", soup.find_all("meta")[3]["content"]).group(1))
+
+    # Infer length of gallery from the number of pages
+    pattern = r">(\d+)</span></a></span></div><div class=\"tag-container field-name\">[\n\r\s]+Uploaded:"
+    length = int(re.search(pattern, result.text).group(1))
+
+    # Locate all pages in the gallery
+    # Pages are linked using <noscript> tags
+    file_extensions = []
+    for match in soup.find_all("noscript"):
+        pattern = r"galleries/(\d+)/.*?\.(.*?)\""
+        result = re.search(pattern, str(match))
+        if result.group(1) == str(internal_gallery_id) and "cover" not in result.group(0):
+            file_extensions.append(result.group(2))
+
+    pages = [NHentaiPage(
+        direct_url=f"https://i.nhentai.net/galleries/{internal_gallery_id}/{i+1}.{file_extensions[i]}",
+        file_extension=file_extensions[i]) for i in range(length)]
+
+    return NHentaiSession(gallery_id=gallery_id,
+                          title=title,
+                          gallery_internal_id=internal_gallery_id,
+                          current_page_number=0,
+                          length=length,
+                          pages=pages)
+
+
+def nhentai_search(query: str, mode: str = "recent") -> NHentaiSearchResult:
+    '''
+    Search for a gallery using a query
+    '''
+    scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+
+    sort_suffixes = {"recent": "", "popular": "&sort=popular"}
+
+    query_url = f"https://nhentai.net/search/?q={query}" + sort_suffixes[mode]
+
+    try:
+        result = scraper.get(query_url)
+    except Exception as e:
+        return NHentaiSearchResult(query=query, num_results=0, error=e)
+
+    soup = BeautifulSoup(result.text, "html.parser")
+    title_block = soup.find_all("h1")[0].text
+
+    if "no" in result:
+        return NHentaiSession(gallery_id=0, exists=False)
+
+    # parse number of results as int
+    # String is of format e.g. "1,780 results"
+    num_results = int(
+        re.search(r"(\d+)", title_block.replace(",", "")).group(1))
+
+    # Infer number of pages from number of results
+    # Each page holds approximately 25 results
+    num_pages = num_results // 25 + 1
+
+    # Cache the first page since we're already on it
+    # Search for all gallery links
+    gallery_numbers = re.findall(r"/g/(\d+)/", result.text)
+
+    return NHentaiSearchResult(query=query, sort_suffix=sort_suffixes[mode], num_results=num_results,
+                               num_pages=num_pages, cached_pages={0: gallery_numbers})
+
+
+def nhentai_search_cursor(search_result: NHentaiSearchResult) -> NHentaiSession:
+    '''
+    Return an Nhentai gallery from a search result given the cursor position.
+    '''
+    if search_result.page_cursor in search_result.cached_pages:
+        gallery_id = search_result.cached_pages[search_result.page_cursor][search_result.page_subcursor]
+        return initialize_session(gallery_id)
+    else:
+        # Need to cache this page
+        query_url = f"https://nhentai.net/search/?q={search_result.query}" + \
+            search_result.sort_suffix + f"&page={search_result.page_cursor+1}"
+
+        scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+        result = scraper.get(query_url)
+        gallery_numbers = re.findall(r"/g/(\d+)/", result.text)
+        search_result.cached_pages[search_result.page_cursor] = gallery_numbers
+        # Update the list
+
+        gallery_id = gallery_numbers[search_result.page_subcursor]
+        return initialize_session(gallery_id)
+
+
+def cover_page_embed(session: NHentaiSession) -> Embed:
+    '''
+    Return embed code for the cover page of the gallery
+    '''
+    embed = Embed(title=session.title + f" ({session.gallery_id})",
+                  url=f"https://nhentai.net/g/{session.gallery_id}/",
+                  color=0xfecbed,
+                  image=session.pages[0].direct_url)
+    embed.set_footer(text=f"Number of Pages: {session.length}")
+    embed.set_author(name="NHentai", url="https://nhentai.net/")
+    return embed
+
+
+if __name__ == "__main__":
+    # Testing script
+    # result = initialize_session(185217)
+    # print(result.pages)
+    nhentai_search("alp")
